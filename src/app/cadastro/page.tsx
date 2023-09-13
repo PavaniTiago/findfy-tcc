0e41260936404f72439bbcae583de99a9bb8b3ed
Tiago Pavani <tiagopavanisalto@hotmail.com>
2023-09-13 01:29:39 -0300

    (feat): add authentication components

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,7 +1,6 @@
 import { Github, Facebook } from "lucide-react";
-import Link from "next/link";
 
-export default function Home() {
+export default function page() {
   return (
     <main className="w-full h-screen bg-purple-950 text-white pb-28">
       <div className="h-full flex flex-col items-center justify-center">
@@ -14,13 +13,14 @@ export default function Home() {
           <label htmlFor="input" className="text-sm font-medium">Email</label>
           <input type="email" placeholder="insira seu e-mail" className="bg-purple-950 rounded px-3 py-2 shadow-lg mb-3"/>
           <label htmlFor="input" className="text-sm font-medium">Senha</label>
-          <input type="password" placeholder="insira sua senha" className="bg-purple-950 rounded px-3 py-2 shadow-lg"/>
-          <button type="submit" className="w-full bg-white hover:bg-white/90 text-black rounded py-1.5 text-md font-semibold mt-4 mb-2 shadow-lg">Entrar</button>
+          <input type="password" placeholder="insira sua senha" className="bg-purple-950 rounded px-3 py-2 shadow-lg mb-3"/>
+          <label htmlFor="input" className="text-sm font-medium">Confirme sua senha</label>
+          <input type="password" placeholder="confirme sua senha" className="bg-purple-950 rounded px-3 py-2 shadow-lg"/>
+          <button type="submit" className="w-full bg-white hover:bg-white/90 text-black rounded py-1.5 text-md font-semibold mt-4 mb-2 shadow-lg">Criar conta</button>
           <div className="flex self-center space-x-8">
             <button className="w-20 flex items-center justify-center mt-3 bg-white hover:bg-white/90 rounded text-black py-1"><Github className="w-6 h-6"/></button>
             <button className="w-20 flex items-center justify-center mt-3 bg-white hover:bg-white/90 rounded text-black py-1"><Facebook className="w-6 h-6"/></button>
           </div>
-          <Link href="/cadastro" className="flex text-sm text-white/70 hover:text-white hover:underline self-center mt-3">Não tem uma conta ainda? crie agora.</Link>
         </form>
           <span className="text-sm text-white/70 mt-1">Feito com ❤ por Find<span className="text-purple-200">Fy</span></span>
       </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Header(){
     return(
-        <header className="w-full h-12 flex items-center bg-purple-950 text-white px-6">
-            <span>LOGO</span>
+        <header className="w-full h-12 flex items-center bg-purple-950 text-white pt-4 px-6">
+            <span className="block">LOGO</span>
             <div className="flex items-center justify-end w-full gap-6">
-                <button className="flex justify-center items-center px-6 py-1 border border-white rounded-md">Entrar</button>
-                <button className="flex justify-center items-center px-6 py-1 border border-white rounded-md">Cadastrar</button>
+                <Link href="/" className="flex justify-center items-center px-6 py-1 border border-white rounded-md">Entrar</Link>
+                <Link href="/cadastro" className="flex justify-center items-center bg-white text-black hover:text-white font-semibold px-6 py-1 hover:border hover:bg-purple-950 border-white rounded-md transition-colors">Cadastrar</Link>
             </div>
         </header>
     )
