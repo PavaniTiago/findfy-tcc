6e6a5d72828077101cd67d5756418851fea8975b
Tiago Pavani <tiagopavanisalto@hotmail.com>
2023-09-25 09:57:24 -0300

    (fix): authOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth"
-import { authOptions } from "@/app/utils/authOption"
+import { authOptions } from "@/utils/authOption"
 
 export const handler = NextAuth(authOptions)
 
