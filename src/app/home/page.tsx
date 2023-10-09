3495669a346f5f717bda0c0704360c7f3b2bdfac
Tiago Pavani <tiagopavanisalto@hotmail.com>
2023-10-09 17:51:40 -0300

    (feat): add recomendation and categories

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -11,7 +11,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Navigation, Pagination, Mousewheel, Keyboard, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import Image from "next/image";
 
 import banner1 from "../../../public/banner.jpg"
@@ -21,6 +21,8 @@ import banner4 from "../../../public/banner4.jpg"
 import banner5 from "../../../public/banner5.jpg"
 import banner6 from "../../../public/banner6.jpg"
 import banner7 from "../../../public/banner7.jpg"
+import CategoriasFilmes from "@/components/categoriasFilmes";
+import Recomendacao from "@/components/recomendação";
 
 export default function Home(){
     const session = useSession();
@@ -52,22 +54,137 @@ export default function Home(){
             <SwiperSlide><Image width={2000} src={banner7} alt="banner de filmes" className="h-[30rem] object-cover"></Image></SwiperSlide>
           </Swiper>
 
-          <div className="flex flex-col w-full px-14 mt-36">
+          <div className="flex flex-col w-full mx-0 self-start px-14 mt-36">
             <h2 className="text-white font-extrabold text-3xl">Filmes e séries recomendados</h2>
             <Swiper
-            slidesPerView={3}
+            breakpoints={{
+              640: {
+                slidesPerView: 4,
+              },
+              2080: {
+                slidesPerView: 5,
+              },
+            }}
             autoplay
             loop={true}
             modules={[Navigation, Pagination, Autoplay]}
-            className="md:w-full mt-14"
+            className="w-full mt-14 self-start"
           >
-            <SwiperSlide><Image width={0} src={banner1} alt="banner de filmes" className="h-[30rem] w-96 rounded-3xl object-cover"></Image></SwiperSlide>
-            <SwiperSlide><Image width={1500} src={banner2} alt="banner de filmes" className="h-[30rem] w-96 rounded-3xl object-cover"></Image></SwiperSlide>
-            <SwiperSlide><Image width={1500} src={banner3} alt="banner de filmes" className="h-[30rem] w-96 rounded-3xl object-cover"></Image></SwiperSlide>
-            <SwiperSlide><Image width={1500} src={banner4} alt="banner de filmes" className="h-[30rem] w-96 rounded-3xl object-cover"></Image></SwiperSlide>
-            <SwiperSlide><Image width={1500} src={banner5} alt="banner de filmes" className="h-[30rem] w-96 rounded-3xl object-cover"></Image></SwiperSlide>
-            <SwiperSlide><Image width={1500} src={banner6} alt="banner de filmes" className="h-[30rem] w-96 rounded-3xl object-cover"></Image></SwiperSlide>
-            <SwiperSlide><Image width={1500} src={banner7} alt="banner de filmes" className="h-[30rem] w-96 rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner1} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner2} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner3} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner4} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner5} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner6} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner7} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+          </Swiper>
+          </div>
+          <div className="flex flex-col w-full mx-0 self-start px-14 mt-36">
+            <h2 className="text-white font-extrabold text-3xl">Categorias de filmes e séries</h2>
+            <Swiper
+            breakpoints={{
+              640: {
+                slidesPerView: 3,
+              },
+              2080: {
+                slidesPerView: 4,
+              },
+            }}
+            autoplay
+            loop={true}
+            modules={[Navigation, Pagination, Autoplay]}
+            className="w-full mt-14 self-start"
+          >
+            <SwiperSlide className="mr-8"><CategoriasFilmes img={banner1} title="romance"/></SwiperSlide>
+            <SwiperSlide className="mr-8"><CategoriasFilmes img={banner1} title="romance"/></SwiperSlide>
+            <SwiperSlide className="mr-8"><CategoriasFilmes img={banner1} title="romance"/></SwiperSlide>
+            <SwiperSlide className="mr-8"><CategoriasFilmes img={banner1} title="romance"/></SwiperSlide>
+            <SwiperSlide className="mr-8"><CategoriasFilmes img={banner1} title="romance"/></SwiperSlide>
+            <SwiperSlide className="mr-8"><CategoriasFilmes img={banner1} title="romance"/></SwiperSlide>
+            <SwiperSlide className="mr-8"><CategoriasFilmes img={banner1} title="romance"/></SwiperSlide>
+          </Swiper>
+          </div>
+          <div className="mt-32">  
+            <Recomendacao 
+              image={banner1} 
+              description="Beleza, riqueza e status definem as pessoas que vivem nos Hamptons, mas uma mulher em busca de vingança pode destruir tudo. Emily Thorne infiltra-se na alta sociedade dos Hamptons para vingar-se das pessoas que incriminaram seu pai anos atrás."
+              episodes={14}
+              temps={4}
+            />
+          </div>
+          <div className="flex flex-col w-full mx-0 self-start px-14 mt-36">
+            <h2 className="text-white font-extrabold text-3xl">Filmes e séries recomendados</h2>
+            <Swiper
+            breakpoints={{
+              640: {
+                slidesPerView: 4,
+              },
+              2080: {
+                slidesPerView: 5,
+              },
+            }}
+            autoplay
+            loop={true}
+            modules={[Navigation, Pagination, Autoplay]}
+            className="w-full mt-14 self-start"
+          >
+            <SwiperSlide className=""><Image width={0} src={banner1} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner2} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner3} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner4} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner5} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner6} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner7} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+          </Swiper>
+          </div>
+          <div className="flex flex-col w-full mx-0 self-start px-14 mt-36">
+            <h2 className="text-white font-extrabold text-3xl">Filmes e séries recomendados</h2>
+            <Swiper
+            breakpoints={{
+              640: {
+                slidesPerView: 4,
+              },
+              2080: {
+                slidesPerView: 5,
+              },
+            }}
+            autoplay
+            loop={true}
+            modules={[Navigation, Pagination, Autoplay]}
+            className="w-full mt-14 self-start"
+          >
+            <SwiperSlide className=""><Image width={0} src={banner1} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner2} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner3} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner4} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner5} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner6} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner7} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+          </Swiper>
+          </div>
+          <div className="flex flex-col w-full mx-0 self-start px-14 mt-36">
+            <h2 className="text-white font-extrabold text-3xl">Filmes e séries recomendados</h2>
+            <Swiper
+            breakpoints={{
+              640: {
+                slidesPerView: 4,
+              },
+              2080: {
+                slidesPerView: 5,
+              },
+            }}
+            autoplay
+            loop={true}
+            modules={[Navigation, Pagination, Autoplay]}
+            className="w-full mt-14 self-start"
+          >
+            <SwiperSlide className=""><Image width={0} src={banner1} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner2} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner3} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner4} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner5} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner6} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
+            <SwiperSlide className=""><Image width={0} src={banner7} alt="banner de filmes" className="h-[405.131px] w-[306.22px] rounded-3xl object-cover"></Image></SwiperSlide>
           </Swiper>
           </div>
         </div>
---
+++ src/components/recomendação.tsx
@@ -0,0 +1,33 @@
+import { Heart } from "lucide-react";
+import Image, { StaticImageData } from "next/image"
+import { useState } from "react";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai"
+
+interface RecomendacaoProps {
+    image: StaticImageData;
+    description: string;
+    episodes: number;
+    temps: number;
+}
+export default function Recomendacao({ image, description, episodes, temps }: RecomendacaoProps) {
+
+    const [liked, setLiked] = useState<boolean>(false)
+
+  return (
+        <div className="flex w-[64rem] items-center justify-center">
+            <Image width={0} alt="imagem de recomendação do dia" src={image} className="h-[425.131px] w-[300.22px] rounded-3xl object-cover -rotate-12 shadow-2xl mr-32"></Image>
+            <div className="flex flex-col">
+              <h2 className="text-white font-extrabold text-3xl text-center mb-8">Recomendação do dia</h2>
+              <p className="text-white text-xl pb-4">{description}</p>
+                <div className="flex gap-12">
+                    <span className="text-white font-bold text-3xl bg-purple-100 py-3 px-12 rounded-xl text-center shadow-lg">{episodes}</span>
+                    <span className="text-white font-bold text-3xl bg-purple-100 py-3 px-12 rounded-xl text-center shadow-lg">{temps}T</span>
+                    {
+                        liked ? <button className="border-0" onClick={() => setLiked(false)}><AiFillHeart size={55}/></button> 
+                        : <button className="border-0" onClick={() => setLiked(true)}><AiOutlineHeart size={55}/></button>
+                    }
+                </div>
+            </div>
+          </div>
+    )
+}
